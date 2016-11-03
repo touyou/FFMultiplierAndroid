95f7e5649220da114033e5aaa6041403adefa4f6
Fujii Yosuke <fujiyou1121@gmail.com>
2016-11-03 19:05:58 +0900

    add custom class

---
+++ app/src/main/java/com/dev/touyou/ffmultiplier/CustomClass/CustomButton.java
@@ -0,0 +1,9 @@
+package com.dev.touyou.ffmultiplier.CustomClass;
+
+import android.widget.Button;
+
+/**
+ * Created by touyou on 2016/11/03.
+ */
+public class CustomButton extends Button {
+}
---
+++ app/src/main/java/com/dev/touyou/ffmultiplier/CustomClass/CustomTextView.java
@@ -0,0 +1,9 @@
+package com.dev.touyou.ffmultiplier.CustomClass;
+
+import android.widget.TextView;
+
+/**
+ * Created by touyou on 2016/11/03.
+ */
+public class CustomTextView extends TextView {
+}
