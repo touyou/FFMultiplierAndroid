c50d10c7176a984cb713e8607954ee453754a160
Fujii Yosuke <fujiyou1121@gmail.com>
2016-11-03 13:42:48 +0900

    initialize admob

--- app/src/main/java/com/dev/touyou/ffmultiplier/MainActivity.java
+++ app/src/main/java/com/dev/touyou/ffmultiplier/MainActivity.java
@@ -2,6 +2,7 @@ package com.dev.touyou.ffmultiplier;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MobileAds.initialize(getApplicationContext(), "ca-app-pub-2853999389157478~5749714867");
     }
 }
