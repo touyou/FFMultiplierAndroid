77d127aeda848e8ba626728f57cacfbda2639eaa
Fujii Yosuke <fujiyou1121@gmail.com>
2016-11-06 22:43:22 +0900

    add onCreateView

--- app/src/main/java/com/dev/touyou/ffmultiplier/Fragments/GameFragment.java
+++ app/src/main/java/com/dev/touyou/ffmultiplier/Fragments/GameFragment.java
@@ -1,7 +1,20 @@
 package com.dev.touyou.ffmultiplier.Fragments;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.dev.touyou.ffmultiplier.R;
+
 /**
  * Created by touyou on 2016/11/06.
  */
-public class GameFragment {
+public class GameFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.fragment_game, container, false);
+    }
 }
