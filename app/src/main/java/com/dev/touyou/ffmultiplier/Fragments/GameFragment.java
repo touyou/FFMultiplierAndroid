f5709a44a80363d5034217b21eb2bb54eca891b1
Fujii Yosuke <fujiyou1121@gmail.com>
2016-11-09 11:51:39 +0900

    write button initialize

--- app/src/main/java/com/dev/touyou/ffmultiplier/Fragments/GameFragment.java
+++ app/src/main/java/com/dev/touyou/ffmultiplier/Fragments/GameFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import com.dev.touyou.ffmultiplier.R;
 
 /**
@@ -12,11 +13,38 @@ import com.dev.touyou.ffmultiplier.R;
  */
 public class GameFragment extends Fragment {
 
+    private Button[] numberButton = new Button[16];
+    private int[] buttonIdList = {
+            R.id.zeroButton, R.id.oneButton, R.id.twoButton, R.id.threeButton,
+            R.id.fourButton, R.id.fiveButton, R.id.sixButton, R.id.sevenButton,
+            R.id.eightButton, R.id.nineButton, R.id.anumButton, R.id.bnumButton,
+            R.id.cnumButton, R.id.dnumButton, R.id.enumButton, R.id.fnumButton
+    };
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         return inflater.inflate(R.layout.fragment_game, container, false);
     }
 
-    public void numberBtn(View v) {}
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        for (int i=0; i<16; i++) {
+            numberButton[i] = (Button) view.findViewById(buttonIdList[i]);
+            numberButton[i].setTag(i);
+            numberButton[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    tappedNumberBtn(view);
+                }
+            });
+        }
+    }
+
+    private void tappedNumberBtn(View v) {
+        int tag = (int) v.getTag();
+    }
 }
