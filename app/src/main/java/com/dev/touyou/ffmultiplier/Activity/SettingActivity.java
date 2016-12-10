2ab225836cc42b9b7261dd9567abefc1e5f6b2ee
Fujii Yosuke <fujiyou1121@gmail.com>
2016-12-10 09:24:10 +0900

    done settings

---
+++ app/src/main/java/com/dev/touyou/ffmultiplier/Activity/SettingActivity.java
@@ -0,0 +1,58 @@
+package com.dev.touyou.ffmultiplier.Activity;
+
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import com.dev.touyou.ffmultiplier.R;
+import com.google.android.gms.ads.identifier.AdvertisingIdClient;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SettingActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private String name;
+
+    private SharedPreferences sp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+
+        editText = (EditText) findViewById(R.id.nameEditText);
+        sp = PreferenceManager.getDefaultSharedPreferences(this);
+        name = sp.getString("name", null);
+        if (name != null) {
+            editText.setText(name);
+        }
+    }
+
+    public void tappedSaveBtn(View v) {
+        name = editText.getText().toString();
+        sp.edit().putString("name", name).commit();
+        // 名前変更時にFirebaseも更新
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference ref = database.getReference();
+        Thread adIdThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                AdvertisingIdClient.Info adInfo;
+                try {
+                    adInfo = AdvertisingIdClient.getAdvertisingIdInfo(SettingActivity.this);
+                    final String id = adInfo.getId();
+                    ref.child("scores").child(id).child("name").setValue(name);
+                } catch (Exception e) {
+                }
+            }
+        });
+        adIdThread.start();
+        finish();
+    }
+}
